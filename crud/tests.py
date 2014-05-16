367505e86b914b2402d22f4d15564dbd40f178d4
Joe21 <joej21@gmail.com>
2014-05-16 14:57:19 -0400

    test added, quick review before going tinto deployment

--- crud/tests.py
+++ crud/tests.py
@@ -1,16 +1,38 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from crud.models import Athlete
 
+class AthleteTest(TestCase):
+    def test_first_name(self):
+        """
+        .first_name will return the correct first name
+        """
+        john_starks = Athlete(first_name="John", last_name="Starks", sport="NBA", recommendation="a")
+        self.assertEqual(john_starks.first_name, "John")
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+    def test_last_name(self):
         """
-        Tests that 1 + 1 always equals 2.
+        .last_name will return the correct last name
         """
-        self.assertEqual(1 + 1, 2)
+        john_starks = Athlete(first_name="John", last_name="Starks", sport="NBA", recommendation="a")
+        self.assertEqual(john_starks.last_name, "Starks")
+
+    def test_sport(self):
+        """
+        .sport will return the correct sport
+        """
+        john_starks = Athlete(first_name="John", last_name="Starks", sport="NBA", recommendation="a")
+        self.assertEqual(john_starks.sport, "NBA")
+
+    def test_recommendation(self):
+        """
+        .recommendation will return the correct recommendation choice
+        """
+        john_starks = Athlete(first_name="John", last_name="Starks", sport="NBA", recommendation="a")
+        self.assertEqual(john_starks.recommendation, "a")
+
+    def test_recommendation_value(self):
+        """
+        .get_recommendation_display() will return the correct value of the recommendation choice
+        """
+        john_starks = Athlete(first_name="John", last_name="Starks", sport="NBA", recommendation="a")
+        self.assertEqual(john_starks.get_recommendation_display(), "Hire Joe IMMEDIATELY!")
