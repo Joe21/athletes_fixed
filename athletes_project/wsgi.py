f5bdf9fe90dbf704a95bc825f85539bf6419acd7
Joe21 <joej21@gmail.com>
2014-05-16 15:52:54 -0400

    ready to try to push

--- athletes_project/wsgi.py
+++ athletes_project/wsgi.py
@@ -14,6 +14,12 @@ framework.
 
 """
 import os
+# Heroku deployment additions
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+# End heroku deployment additions
+
+application = Cling(get_wsgi_application())
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
